7632b347cac71a3fd88209ecd29b9957e557db9f
khaled <khaledgamal1345@gmail.com>
2023-11-01 23:25:59 +0200

    created catalog templates and served media files

--- shop/views.py
+++ shop/views.py
@@ -16,4 +16,4 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id , slug):
     product = get_object_or_404(Product, id=id, slug=slug, available= True)
-    return render(request, 'shop/product/detail.html', {'product', product})
+    return render(request, 'shop/product/detail.html', {'product': product})
