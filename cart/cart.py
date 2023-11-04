69e15ebf8abd169b30d8f3cc39faad7252d68358
khaled <khaledgamal1345@gmail.com>
2023-11-04 17:03:01 +0200

    finished the functionality for the cart

--- cart/cart.py
+++ cart/cart.py
@@ -31,3 +31,44 @@ class Cart:
     # tells django that session is modified so save it
     def save(self):
             self.session.modified=True
+
+    def remove(self,product):
+        """
+        Remove a product from the cart.
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def __iter__(self):
+        """
+        Iterate over the items in the cart and get the products
+        from the database.
+        """
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in = product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price']= Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        """
+        Count all items in the cart
+        """
+        return sum(item['quantity'] for item in self.cart.values())
+    
+    def get_total_price(self):
+        return sum(item['price'] * item['quantity'] for item in self.cart.values())
+    
+    def clear(self):
+        """
+        remove cart from session
+        """
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
+
