994a5e6f16f5bbe00a66d8acded7fd2f3278c658
khaled <khaledgamal1345@gmail.com>
2023-11-08 19:36:59 +0200

    added the templates for creating order

--- orders/forms.py
+++ orders/forms.py
@@ -2,5 +2,6 @@ from django import forms
 from .models import Order
 
 class OrderCreateForm(forms.ModelForm):
-    model = Order
-    fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
--- orders/views.py
+++ orders/views.py
@@ -10,8 +10,11 @@ def order_create(request):
         if form.is_valid():
             order = form.save()
             for item in cart:
-                OrderItem.objects.create(order=order , product = item['product'], quantity = item['quantity'],)
-                cart.clear()
+                OrderItem.objects.create(order=order ,
+                                        product = item['product'],
+                                        price = item['price'],
+                                        quantity = item['quantity'])
+            cart.clear()
             return render(request, 'orders/order/created.html' ,{'order': order})
 
     else:
