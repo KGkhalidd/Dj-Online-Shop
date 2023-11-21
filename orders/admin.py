1d3db716fc280164bbc6f7e1f2c907b6330aa154
khaled gamal <khaledgamal1345@gmail.com>
2023-11-21 19:48:11 +0200

    created custom action 'export orders to csv' for admin site

--- orders/admin.py
+++ orders/admin.py
@@ -2,7 +2,35 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Order, OrderItem
 from django.utils.safestring import mark_safe
+import csv
+import datetime
+from django.http import HttpResponse
 
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    # Create an HTTP response with CSV content type
+    response = HttpResponse(content_type='text/csv')
+    # Set the content disposition header for download
+    response['Content-Disposition'] = content_disposition
+    # Create a CSV writer using the response
+    writer = csv.writer(response)
+    # Get the fields of the model (excluding many-to-many and one-to-many fields)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Write the header row with field names
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        # Write the data row to the CSV file
+        writer.writerow(data_row)
+    # Return the HTTP response with CSV content
+    return response
 
 def order_payment(obj):
     url = obj.get_stripe_url()
@@ -24,3 +52,4 @@ class OrderAdmin(admin.ModelAdmin):
     'address', 'postal_code', 'city', 'paid', order_payment, 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
