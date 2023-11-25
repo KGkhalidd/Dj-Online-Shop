d58b50d10654fdb9ef45b511c4ad350dd8a78ad5
khaled gamal <khaledgamal1345@gmail.com>
2023-11-25 17:46:57 +0200

    created app and added model for coupon system

---
+++ coupons/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+# Create your models here.
+
+class Coupon(models.Model):
+    code = models.CharField( max_length=50, unique= True)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    discount = models.IntegerField(validators=[MinValueValidator(0),
+                                    MaxValueValidator(100)],
+                                    help_text='Percentage value (0 to 100)')
+    active = models.BooleanField()
+    def __str__(self):
+        return self.code
--- myshop/settings.py
+++ myshop/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
     'payment.apps.PaymentConfig',
+    'coupons.apps.CouponsConfig',
 ]
 
 MIDDLEWARE = [
