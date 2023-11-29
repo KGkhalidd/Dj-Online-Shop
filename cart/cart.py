d2e637f99db4a272103e93cc283144a14ea7a303
khaled gamal <khaledgamal1345@gmail.com>
2023-11-29 19:36:42 +0200

    added cart functionality to handle a coupon applied to the current session and apply the discount of that coupon

--- cart/cart.py
+++ cart/cart.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from shop.models import Product
+from coupons.models import Coupon
 
 class Cart:
     def __init__(self, request):
@@ -74,5 +75,22 @@ class Cart:
         """
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return None
+    
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount/ Decimal(100) * self.get_total_price())
+        return Decimal(0)
+    
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
 
 
