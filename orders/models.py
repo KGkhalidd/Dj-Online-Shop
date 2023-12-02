1c0ce1cb35a280fa74e6a4de0aaeaff1678a6f99
khaled gamal <khaledgamal1345@gmail.com>
2023-12-02 04:25:43 +0200

    added methods in order model for calc the total cost before and after the discount

--- orders/models.py
+++ orders/models.py
@@ -30,8 +30,18 @@ class Order(models.Model):
     def __str__(self):
         return f'Order {self.id}'
     
-    def get_total_cost(self):
+    def get_total_cost_before_discount(self):
         return sum(item.get_cost() for item in self.items.all())
+    
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount/ Decimal(100))
+        return Decimal(0)
+    
+    def get_total_cost(self):
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
     
     def get_stripe_url(self):
         if not self.stripe_id:
